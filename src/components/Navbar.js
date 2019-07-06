2938240a08405c863ff1f5b501a24a48bb028e69
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-07-06 15:29:57 +0530

    Done with basic setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class Navbar extends Component {
+  render(){
+    return(
+      <div>
+      Welcome to Navbar Component.
+      </div>
+    );
+  }
+}
