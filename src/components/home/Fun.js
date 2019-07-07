f67258d2c202e500a9977930a1c4876c52eecc94
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-07-07 18:00:22 +0530

    Mixins done of header and section

--- src/components/home/Fun.js
+++ src/components/home/Fun.js
@@ -4,7 +4,21 @@ export default class Fun extends Component {
   render(){
     return(
       <div>
-        Welcome to Fun Component.
+      <div className="container mt-5 border-top">
+        <aside className="mt-5 pt-5">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="text-center display-4 mb-4">Developers Zone</h1>
+              <h6 className="text-muted text-center">FrontEnd Devs can find different gradients and font families here !</h6>
+            </div>
+            <div className="w-100 my-3"></div>
+            <div className="col-lg-6"></div>
+            <div className="col-lg-6"></div>
+            <div className="col-lg-6"></div>
+            <div className="col-lg-6"></div>
+          </div>
+        </aside>
+      </div>
       </div>
     );
   }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
       <div>
       <Header/>
       <Section/>
-      {/*<Fun/>*/}
+      <Fun/>
       </div>
     );
   }
