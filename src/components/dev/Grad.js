8c1cfd68074ce9f9a7d6dfc35c987310a08adaed
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-07-09 22:40:42 +0530

    Little touch to gradients

--- src/components/dev/Grad.js
+++ src/components/dev/Grad.js
@@ -1,7 +1,32 @@
 import React,{Component} from 'react';
 import axios from 'axios';
+import Lf from '../Lf';
 
 export default class Grad extends Component {
+  componentWillMount(){
+      if(localStorage.getItem('user'))
+      this.setState({token:localStorage.getItem('user')});
+  }
+  constructor(props){
+    super(props);
+    this.state = {
+      token:''
+    };
+  }
+  render(){
+    let deliver;
+    if(this.state.token)
+    deliver=<RGrad/>
+    else deliver=<Lf/>
+    return(
+      <div>
+        {deliver}
+      </div>
+    );
+  }
+}
+
+class RGrad extends Component {
   componentDidMount(){
     axios({
       url:'https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json'
@@ -31,7 +56,7 @@ export default class Grad extends Component {
       </div>
       <div className="row mt-4">{
           this.state.colors.map((a,i)=>
-        <div key={i} className="col-2 mx-3 mt-4 rounded text-light d-flex justify-content-center align-items-center"
+        <div key={i} className="col-lg-2 col-offset-3 col-6 mx-5 mx-lg-3 mt-4 rounded text-light d-flex justify-content-center align-items-center"
         style={{"background":`linear-gradient(${a.colors[0]},${a.colors[1]})`,
         "height":"220px"}}>
         <p className="text-center text-light text-uppercase">{`${a.colors[0]} -> ${a.colors[1]}`}</p>
